3aeb9fdb6041d066881827301af7d2e0a708ad33
Joseph Malaba <joseph.malaba@gmail.com>
2015-06-12 03:03:44 +0300

    Friday aarly morning, added GUI

--- ClassCreator.php
+++ ClassCreator.php
@@ -18,24 +18,6 @@ class ClassCreator {
         //$this->con = new DynamicConnection();
     }
     
-    //other methods
-        //conditioning functions
-        
-        public function addIf($condition,$outcome){
-            $statemente = "if( ".$condition." ){\n\t\t".$outcome.";\n\t}";
-            return $statemente;
-        }
-        
-        public function addElse($outcome){
-            $statemente = "else{ ".$outcome.";}";
-            return $statemente;
-        }
-        
-        public function addElseIf($condition,$outcome){
-            $statemente = "elseif( ".$condition." ){\n\t\t".$outcome."; \n\t}";
-            return $statemente;
-        }
-    
     //conmection function
     public function startConnection($host,$user,$pwd,$db){
         $this->con = new DynamicConnection($host,$user,$pwd,$db);
@@ -56,107 +38,53 @@ class ClassCreator {
     
     //folder (diroctory creating functions)
     public function createDirectory($directory){
-        if(mkdir($directory)){
+        if(@mkdir($directory)){
             return true;
         }else{
             return false;
         }
     }
     
-    //function to add setters
-    public function addSetters($tableName){
-        $sql = "DESCRIBE $tableName";
-        $this->con->execute_query($sql);
-        $tableresult = $this->con->get_result();
-
-        $setter = '';
-        foreach($tableresult as $row){
-            $setter.= "public function set".ucfirst(strtolower($row['Field']))."($".strtolower($row['Field'])."){\n";
-            $setter.= "\t$"."updateQr = \"UPDATE $tableName SET ".$row['Field']."='$".strtolower($row['Field'])."'\";\n\n";
-            
-            $condition = " $"."this->con->execute_query($"."updateQr) ";
-            
-            $outcome = "\t$"."this->".strtolower($row['Field'])."=$".strtolower($row['Field']).";\n";
-            $setter.= $this->addIf($condition,$outcome);
-            
-            $setter.= "}\n\n";
-        }
-        return $setter;
-    }
-    
-    //function to add getters
-    public function addGetters($tableName){
-        $getter = '';
-        foreach($tableresult as $row){
-            $getter.= "public function get".ucfirst(strtolower($row['Field']))."(){\n";
-            $getter.= "\t return $"."this->".strtolower($row['Field']).";\n";
-            $getter.= "}\n\n";
-        }
-        return $getter;
-    }
-    
     //funtion to add properties
     public function addProperty($tableName){
-        $sql = "DESCRIBE $tableName";
+        $sql_key = "SHOW KEYS FROM $tableName WHERE Key_name='PRIMARY'";
+		$this->con->execute_query($sql_key);
+		$keyresult = $this->con->get_result();
+		$key = '';
+		foreach($keyresult as $row){
+            $key = $row['Column_name'];
+        }
+		
+		$sql = "DESCRIBE $tableName";
         $this->con->execute_query($sql);
         $tableresult = $this->con->get_result();
 
-        $property = '';
+        $property = "\t//Database table constants\n";
+		$property.="\tconst DB_TABLE = '".$tableName."';\n\tconst DB_TABLE_PK = '".$key."';\n\n";
+        $property .="\t//Properties\n";
         foreach($tableresult as $row){
-            $property.= "\t\\\\Properties\n";
             $property.= "\tprivate $".lcfirst($row['Field']).";\n";
-            $property.= "\tprivate $"."con".";\\\\Connection Object \n";
-            $property.= "\n\n";
         }
-        return $property;
+        return $property."";
     }
     
-    //function to add load($id) method, for fetchting and setting properties from the database
-    public function addLoader($tableName){
-        $sql = "DESCRIBE $tableName";
+    //a function to call all other functions and create all classes from db...
+    public function createClases(){
+        $sql = "SHOW TABLES";
         $this->con->execute_query($sql);
         $tableresult = $this->con->get_result();
-        
-        //Checking for Primary Keys
-        $key = '';
-        $keyCondtion='';
-        $keyVal = 0;
-        $properties='';
         foreach($tableresult as $row){
-            if($row['Key']=='PRI'){
-                if($keyVal==0){
-                    $key.=" $".$row['Field']." ";
-                    $keyCondtion.=" WHERE ".$row['Field']."='$".strtolower($row['Field'])."'\";\n\n";
-                }else{
-                    $keyCondtion.=" AND ".$row['Field']."='$".strtolower($row['Field'])."'\";\n\n";
-                    $key.=",$".$row['Field']." ";
-                }
-            }
-            $properties.= "\t\t\t$"."this->".lcfirst($row['Field'])."= $"."row[\"".$row['Field']."\"];\n";
-            $keyVal++;
+            $properties = $this->addProperty($row[0]);
+            
+            $filecontent = "<?php\n/*\n*Created By Malaba model builder tool for codeigniter\n* ".Date('d-M-Y')."\n* Joseph Malaba\n*/\n\n// a class reflecting table ".$row[0]."\n";
+            $filecontent.="class ".ucfirst(str_replace('_','',str_replace('tbl','',strtolower($row[0]))))." extends MY_Model";
+            $filecontent.="{\n".$properties."}\n?>";
+            
+            $directory = "./ClassCreator";
+            $this->createDirectory($directory);
+            $fileName = $directory."/".lcfirst(str_replace('_','',str_replace('tbl','',strtolower($row[0]))))."";
+            $this->createFile($fileName,$filecontent);
         }
-        
-        $loader = '';
-        foreach($tableresult as $row){
-            $loader.= "public function load($key){\n"; //first add keys(primary keys if any) in parameter list
-            $loader.= "\t$"."selectQr = \"SELECT * FROM $tableName $keyCondtion";
-            
-            $condition = " $"."this->con->execute_query($"."updateQr) ";
-            
-            $outcome = "\t$"."this->".strtolower($row['Field'])."=$".strtolower($row['Field']).";\n";
-            $setter.= $this->addIf($condition,$outcome);
-            
-            $setter.= "}\n\n";
-        }
-        return $loader;
     }
-    
-    
-    //function to add create($p1,$p2....) method which will be used for insert
-        
-    //setters (for update)
-    
-    //getters (for select)
-    
 }
 ?>
